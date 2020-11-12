97b6f9c4cafa7d03f73080e20e86c356e3436cf6
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-12 19:50:13 +0000

    Create utils.js

---
+++ assets/utils.js
@@ -0,0 +1,25 @@
+const [small, medium] = [15, 25]
+
+const styleVariables = {
+	grey: '#565656',
+	purple: '#0f0464',
+	greyWhite: '#eeeeee',
+	spacing: 20,
+	radius: 15,
+	baseFontSz: 16,
+	midFontSz: 22,
+
+	iconSmall: {
+		width: small,
+		height: small,
+	},
+
+	iconMedium: {
+		width: medium,
+		height: medium,
+	},
+};
+
+export {
+	styleVariables
+}
